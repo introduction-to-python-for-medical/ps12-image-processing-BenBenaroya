fdcec8aa2b17354c5705d66867f9192e918fa5ed
BenBenaroya <dbb161199@gmail.com>
2025-01-27 13:26:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+load_image('lena (1).jpg')
+clean_image = median(arr_image, ball(6))
+edge_detection(clean_image)
 threshold = 140
 binary_image = edged_image > threshold
 binary_image = binary_image.astype(np.uint8) * 255
