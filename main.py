51026a2aec876355dc328adf8e804cf3edef3670
BenBenaroya <dbb161199@gmail.com>
2025-01-27 12:42:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+image = load_image('lean (1).jpg')
+clean_image = median(arr_image, ball(3))
+edge_detection(image)
 
+threshold = 90
+binary_image = edged_image > threshold
+binary_image = binary_image.astype(np.uint8) * 255
+edge_image = Image.fromarray(binary_image)
+edge_image.save('my_edges.png')
